e1e2cf2ead554d042b53dfc36b3fcc67e506b513
Jeffrey Harding <jeffreyh202@gmail.com>
2020-06-20 18:47:20 -0600

    js updates

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,19 @@
 var startBtn = document.getElementById("start");
 var questionBox = document.querySelector("#Qs");
 var nextBtn = document.querySelector("#next");
-var answerBtns = document.querySelector(".ans");
+var answerBtnA = document.querySelector("#A");
+var answerBtnB = document.querySelector("#B");
+var answerBtnC = document.querySelector("#C");
+var answerBtnD = document.querySelector("#D");
 
-var currentQ = 0;
-
-//Questions
-var questionArray = ["Where in the HTML does the link to an external Javascript file belong?", "What is the Emmet Abbreviation to call the basic structure of an HTML document?", 
-"What is an API?", "Which of the following is not a Data Type in Javascript?"];
 
 //Event Listeners
 startBtn.addEventListener("click", startQuiz);
 nextBtn.addEventListener("click", next);
-answerBtns.addEventListener("click", answer);
+answerBtnA.addEventListener("click", answerA);
+answerBtnB.addEventListener("click", answerB);
+answerBtnC.addEventListener("click", answerC);
+answerBtnD.addEventListener("click", answerD);
 
 
 function startQuiz() {
@@ -25,37 +26,61 @@ function next() {
     console.log("next")
     updateQ()
     showA()
+    showB()
+    showC()
+    showD()
 }
 
 function updateQ() {
-    // console.log("show")
+    var questionArray = ["Where in the HTML does the link to an external Javascript file belong?", "What is the Emmet Abbreviation to call the basic structure of an HTML document?", 
+    "What is an API?", "Which of the following is not a Data Type in Javascript?"]; 
+
+    var currentQ = 0;
+
     for(var i=0, len = questionArray.length, text = ""; i < len; i++){
         text = questionArray[i]}
         questionBox.innerHTML = text;
     }
 
 function showA() {
+    var answerArray = ["In the head section", "$", "Automated Program Information", "String"];
 
-}
-function answer() {
-    console.log("answer")
-}
-
-function displayQuestion(question) {
-
+    for(var i=0, len = answerArray.length, text = ""; i < len; i++){
+        text = answerArray[i]}
+        answerBtnA.innerHTML = text;
 }
 
+function showB() {
+    var answerArrayB = ["At the bottom of the body", "&&", "Antithetical Purpose Interaction", "Array"];
 
+    for(var i=0, len = answerArrayB.length, text = ""; i < len; i++){
+        text = answerArrayB[i]}
+        answerBtnB.innerHTML = text;
+}
+function showC() {
+    var answerArrayC = ["You cannot link to external javascript files", "<>", "Application Programming Interface", "Sentence",];
 
+    for(var i=0, len = answerArrayC.length, text = ""; i < len; i++){
+        text = answerArrayC[i]}
+        answerBtnC.innerHTML = text;
+}
+function showD() {
+    var answerArrayD = ["Anywhere you want to put it", "!", "Antipodal Pataphysical Idiocy", "Number"];
 
+    for(var i=0, len = answerArrayD.length, text = ""; i < len; i++){
+        text = answerArrayD[i]}
+        answerBtnD.innerHTML = text;
+}
 
-
-// ["In the head section", "At the bottom of the body", 
-//     "You cannot link to external javascript files", "Anywhere you want to put it"], "At the bottom of the body"),
-
-//     ["$", "&&", "<>", "!",], "!"),
-
-//     ["Automated Program Information", "Antithetical Purpose Interaction", "Application Programming Interface", "Antipodal Pataphysical Idiocy"],
-//     "Application Programming Interface"),
-
-//     ["String", "Array", "Sentence", "Number"], "Sentence")
+function answerA() {
+    console.log("answerA")
+}
+function answerB() {
+    console.log("answerB")
+}
+function answerC() {
+    console.log("answerC")
+}
+function answerD() {
+    console.log("answerD")
+}
